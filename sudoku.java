8f3d7b6049d0a0b1f52f055b31faccc28bdb1449
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-08-29 22:25:09 +0530

    Create sudoku.java

---
+++ sudoku.java
@@ -0,0 +1,10 @@
+package sudoku;
+/**
+ * Define the named constants used in many classes.
+ */
+public class SudokuConstants {
+   /** Size of the board */
+   public static final int GRID_SIZE = 9;
+   /** Size of sub-grid of the board */
+   public static final int SUBGRID_SIZE = 3;
+}
