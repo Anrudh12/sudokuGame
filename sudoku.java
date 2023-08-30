8490758ca747e955187918dc46c81e5cf179e158
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-08-30 21:20:45 +0530

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -8,3 +8,11 @@ public class SudokuConstants {
    /** Size of sub-grid of the board */
    public static final int SUBGRID_SIZE = 3;
 }
+public enum CellStatus {
+   GIVEN,         // clue, no need to guess
+   TO_GUESS,      // need to guess - not attempted yet
+   CORRECT_GUESS, // need to guess - correct guess
+   WRONG_GUESS    // need to guess - wrong guess
+      // The puzzle is solved if none of the cells have 
+      //  status of TO_GUESS or WRONG_GUESS
+}
