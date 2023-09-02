9d042790f18a2c19f7c112ae27d69040be1b667a
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-09-02 23:07:28 +0530

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -2,6 +2,9 @@ package sudoku;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * Define the named constants used in many classes.
  */
@@ -137,4 +140,75 @@ public class Puzzle {
    }
 
    //(For advanced students) use singleton design pattern for this class
+}
+public class GameBoardPanel extends JPanel {
+   private static final long serialVersionUID = 1L;  // to prevent serial warning
+
+   // Define named constants for UI sizes
+   public static final int CELL_SIZE = 60;   // Cell width/height in pixels
+   public static final int BOARD_WIDTH  = CELL_SIZE * SudokuConstants.GRID_SIZE;
+   public static final int BOARD_HEIGHT = CELL_SIZE * SudokuConstants.GRID_SIZE;
+                                             // Board width/height in pixels
+
+   // Define properties
+   /** The game board composes of 9x9 Cells (customized JTextFields) */
+   private Cell[][] cells = new Cell[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+   /** It also contains a Puzzle with array numbers and isGiven */
+   private Puzzle puzzle = new Puzzle();
+
+   /** Constructor */
+   public GameBoardPanel() {
+      super.setLayout(new GridLayout(SudokuConstants.GRID_SIZE, SudokuConstants.GRID_SIZE));  // JPanel
+
+      // Allocate the 2D array of Cell, and added into JPanel.
+      for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
+         for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
+            cells[row][col] = new Cell(row, col);
+            super.add(cells[row][col]);   // JPanel
+         }
+      }
+
+      // [TODO 3] Allocate a common listener as the ActionEvent listener for all the
+      //  Cells (JTextFields)
+      // .........
+
+      // [TODO 4] Adds this common listener to all editable cells
+      // .........
+
+      super.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+   }
+
+   /**
+    * Generate a new puzzle; and reset the gameboard of cells based on the puzzle.
+    * You can call this method to start a new game.
+    */
+   public void newGame() {
+      // Generate a new puzzle
+      puzzle.newPuzzle(2);
+
+      // Initialize all the 9x9 cells, based on the puzzle.
+      for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
+         for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
+            cells[row][col].newGame(puzzle.numbers[row][col], puzzle.isGiven[row][col]);
+         }
+      }
+   }
+
+   /**
+    * Return true if the puzzle is solved
+    * i.e., none of the cell have status of TO_GUESS or WRONG_GUESS
+    */
+   public boolean isSolved() {
+      for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
+         for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
+            if (cells[row][col].status == CellStatus.TO_GUESS || cells[row][col].status == CellStatus.WRONG_GUESS) {
+               return false;
+            }
+         }
+      }
+      return true;
+   }
+
+   // [TODO 2] Define a Listener Inner Class for all the editable Cells
+   // .........
 }
