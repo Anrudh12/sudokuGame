b38a72e3c45f6f5287dd522743268f81509cb8ab
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-09-03 23:17:11 +0530

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -212,3 +212,36 @@ public class GameBoardPanel extends JPanel {
    // [TODO 2] Define a Listener Inner Class for all the editable Cells
    // .........
 }
+public class SudokuMain extends JFrame {
+   private static final long serialVersionUID = 1L;  // to prevent serial warning
+
+   // private variables
+   GameBoardPanel board = new GameBoardPanel();
+   JButton btnNewGame = new JButton("New Game");
+
+   // Constructor
+   public SudokuMain() {
+      Container cp = getContentPane();
+      cp.setLayout(new BorderLayout());
+
+      cp.add(board, BorderLayout.CENTER);
+
+      // Add a button to the south to re-start the game via board.newGame()
+      // ......
+
+      // Initialize the game board to start the game
+      board.newGame();
+
+      pack();     // Pack the UI components, instead of using setSize()
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
+      setTitle("Sudoku");
+      setVisible(true);
+   }
+
+   /** The entry main() entry method */
+   public static void main(String[] args) {
+      // [TODO 1] Check "Swing program template" on how to run
+      //  the constructor of "SudokuMain"
+      // .........
+   }
+}
