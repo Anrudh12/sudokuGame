08f56018b4e6a9799b9ac457c6266d3306bdd2c0
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-09-04 23:18:36 +0530

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -245,3 +245,35 @@ public class SudokuMain extends JFrame {
       
    }
 }
+ // [TODO 2] Define a Listener Inner Class for all the editable Cells
+   private class CellInputListener implements ActionListener {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         // Get a reference of the JTextField that triggers this action event
+         Cell sourceCell = (Cell)e.getSource();
+		 
+         // Retrieve the int entered
+         int numberIn = Integer.parseInt(sourceCell.getText());
+         // For debugging
+         System.out.println("You entered " + numberIn);
+
+         /*
+          * [TODO 5] (later - after TODO 3 and 4)
+          * Check the numberIn against sourceCell.number.
+          * Update the cell status sourceCell.status,
+          * and re-paint the cell via sourceCell.paint().
+          */
+          //if (numberIn == sourceCell.number) {
+          //   sourceCell.status = CellStatus.CORRECT_GUESS;
+          //} else {
+          //   ......
+          //}
+          //sourceCell.paint();   // re-paint this cell based on its status
+
+         /*
+          * [TODO 6] (later)
+          * Check if the player has solved the puzzle after this move,
+          *   by calling isSolved(). Put up a congratulation JOptionPane, if so.
+          */
+      }
+   }
